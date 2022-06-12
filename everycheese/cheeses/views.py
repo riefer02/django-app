012f1c2eff99e9517eda65c98a886a5065bd14a1
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-06-11 19:35:12 -0500

    add cheese update view, modify template

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Cheese
 # Create your views here.
 
@@ -27,3 +27,14 @@ class CheeseCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+
+class CheeseUpdateView(LoginRequiredMixin, UpdateView):
+    model = Cheese
+    fields = [
+        'name',
+        'description',
+        'firmness',
+        'country_of_origin'
+    ]
+    action = "Update"
